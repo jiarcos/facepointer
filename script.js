853eaece21d786f433c924b29fbd252c3c3731bc
jiarcos <jaimearcos@Modyos-iMac.local>
2016-05-12 13:17:49 -0300

    Commit archivos
    Respaldo de archivos html, css y js de proyecto grilla de seguimiento

---
+++ script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+	// first-line
+	$('.first-line div:nth-child(1)').mouseover(function(){
+		$('.head img').css('margin-top', -1800);
+	});
+	$('.first-line div:nth-child(2)').mouseover(function(){
+		$('.head img').css('margin-top', -2700);
+	});
+	$('.first-line div:nth-child(3)').mouseover(function(){
+		$('.head img').css('margin-top', -3600);
+	});
+	// second-line
+	$('.second-line div:nth-child(1)').mouseover(function(){
+		$('.head img').css('margin-top', -900);
+	});
+	$('.second-line div:nth-child(2)').mouseover(function(){
+		$('.head img').css('margin-top', -5400);
+	});
+	$('.second-line div:nth-child(3)').mouseover(function(){
+		$('.head img').css('margin-top', 5400);
+	});
+	// last-line
+	$('.last-line div:nth-child(1)').mouseover(function(){
+		$('.head img').css('margin-top', 900);
+	});
+	$('.last-line div:nth-child(2)').mouseover(function(){
+		$('.head img').css('margin-top', 1800);
+	});
+	$('.last-line div:nth-child(3)').mouseover(function(){
+		$('.head img').css('margin-top', 2700);
+	});
+	$('.last-line div:nth-child(4)').mouseover(function(){
+		$('.head img').css('margin-top', 3600);
+	});
+	$('.last-line div:nth-child(5)').mouseover(function(){
+		$('.head img').css('margin-top', 4500);
+	});
+
+});
